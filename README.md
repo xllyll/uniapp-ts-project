b0e7f53e237a73eda52d2757b248726d097d29cf
xllyll <757271398@qq.com>
2025-01-10 13:58:32 +0800

    文件修正

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # uniapp ts project
+
+## 运行
+~~~bash
+
+git clone https://github.com/xllyll/uniapp-ts-project.git
+
+cd uniapp-ts-project
+
+npm install
+
+npm run dev:h5
+
+~~~
+
+## 结构说明
+~~~
+auto-imports.d.ts 自动引入生成文件
+components.d.ts 自动引入组件生成文件
+~~~
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+// 自动导入插件
 import AutoImport from "unplugin-auto-import/vite";
+// 自动导入组件
 import Components from 'unplugin-vue-components/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
