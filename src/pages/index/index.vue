d5d29b4e3f02824a98553d8363bb9accb564cd3c
xllyll <757271398@qq.com>
2025-01-22 12:00:09 +0800

    文件修正

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,37 +9,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
-onLoad(()=>{
-  alert("-----")
-})
+  import { ref } from 'vue'
+  const title = ref('Hello')
+  onLoad(()=>{
+    alert("-----")
+  })
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+  }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
 </style>
